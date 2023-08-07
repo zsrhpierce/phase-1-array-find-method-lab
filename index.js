3b3c17e4c9e1ee16da2b783dbcd9612d77bc629d
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-06 20:03:20 -0600

    array

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function superbowlWin(arrayArgument){   
+    
+   let winObject = arrayArgument.find(isWin);
+   if (winObject === undefined) {
+    return undefined;
+   
+   }
+   return winObject.year;
+   
+   
+}
+
+function isWin(element, index, array){
+    return element.result === 'W';
+    
+}
+
